384d4041cdf1a31c4e5686a2ee7bbf829438b770
Lucas angelo <angelolucas374@gmail.com>
2024-03-14 19:08:19 -0300

    feat:Home criada mais atualização

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -8,20 +8,21 @@ import { ETheme } from '../../enums/ETheme.enum';
 })
 export class HeaderComponent implements OnInit{
  public icon:string = ETheme.ICON_MOON;
+ public textTheme: string = ETheme.TEXT_MOON;
 
  constructor() {}
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+  ngOnInit(): void {}
 
   public toggle(){
    const theme = document.body.classList.toggle('dark-theme');//Quando clicar no botão vai ficar dark vindo da class no css
-   if(theme) {
-    return(this.icon = ETheme.ICON_SUN);
-   } else {
-    return (this.icon = ETheme.ICON_MOON)
-   }
-  }
 
+   if(theme) {
+    this.textTheme = ETheme.TEXT_SUN;
+    return(this.icon = ETheme.ICON_SUN);
+   }
+   this.textTheme = ETheme.TEXT_MOON;
+    return (this.icon = ETheme.ICON_MOON)
+
+  }
 }
