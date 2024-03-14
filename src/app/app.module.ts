99f11111f3547f66e7e029960e81cf09faab5c95
Lucas angelo <angelolucas374@gmail.com>
2024-03-14 19:09:43 -0300

    feat:Pagina sobre criada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { HomeComponent } from './core/pages/components/home/home.component';
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
