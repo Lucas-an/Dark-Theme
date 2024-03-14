29f6b4e263989e570520e80b93468694ec146a79
Lucas angelo <angelolucas374@gmail.com>
2024-03-14 17:51:25 -0300

    feat: atualização button

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ETheme } from '../../enums/ETheme.enum';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+ public icon:string = ETheme.ICON_MOON;
+
+ constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public toggle(){
+   const theme = document.body.classList.toggle('dark-theme');//Quando clicar no botão vai ficar dark vindo da class no css
+   if(theme) {
+    return(this.icon = ETheme.ICON_SUN);
+   } else {
+    return (this.icon = ETheme.ICON_MOON)
+   }
+  }
 
 }
